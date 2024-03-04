8992f48996db890659aac2a6a79323c12dc7cf65
TugberkAta <atatugberk@yahoo.com>
2024-03-04 13:27:22 +0200

    Add new component for nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
+import Navigation from "@/common/components/Navigation";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-black h-screen w-screen">
+        <Navigation />
+      </div>
+    </>
+  );
 }
---
+++ common/components/Navigation.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+// Array of every tab to be shown in the nav bar
+let tabs = [
+  { id: "Homepage" },
+  { id: "Projects" },
+  { id: "Resume" },
+  { id: "Content" },
+];
+
+export default function Navigation() {
+  // State for updating the active tab
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  return (
+    <div
+      className="flex justify-center items-center text-lg w-full h-16 text-white"
+      onMouseLeave={() => setActiveTab(tabs[0].id)}
+    >
+      <div className="w-96">
+        <div className="flex justify-center gap-4">
+          {/* Map the tab array into link components */}
+          {tabs.map((tab) => (
+            <a
+              key={tab.id}
+              href=""
+              className={"relative"}
+              onMouseEnter={() => setActiveTab(tab.id)}
+            >
+              {tab.id}
+              {activeTab === tab.id && (
+                /*This component uses layoutId to animate between positions*/
+                <motion.div
+                  layoutId="underline"
+                  className="absolute bg-white w-full h-[0.10rem]"
+                  transition={{ duration: 0.7, type: "spring" }}
+                ></motion.div>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
