565a81f0926231a885d0a416b6a3af4ae658c426
TugberkAta <atatugberk@yahoo.com>
2024-03-04 15:35:37 +0200

    Add new shader component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import LightShow from "@/common/components/LightShow";
 import Navigation from "@/common/components/Navigation";
+
 export default function Home() {
   return (
     <>
       <div className="bg-black h-screen w-screen">
         <Navigation />
+        <LightShow />
       </div>
     </>
   );
