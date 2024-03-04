ab7eaf1150165851d1bf69815108b0db0d745d66
TugberkAta <atatugberk@yahoo.com>
2024-03-04 20:11:44 +0200

    Add new about me section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import AboutMe from "@/common/components/AboutMe";
 import LightShow from "@/common/components/LightShow";
 import Navigation from "@/common/components/Navigation";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-black h-screen w-screen">
+      <div className="bg-black h-fit w-screen">
         <Navigation />
         <LightShow />
+        <AboutMe />
       </div>
     </>
   );
