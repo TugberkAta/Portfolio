28c1cdd4185dd678eccb72367d1814ab90af1100
TugberkAta <atatugberk@yahoo.com>
2024-03-05 12:21:30 +0200

    Add a opacity effect to icons that gradiually makes them appear one after another
    Clean up some code and seperate concerns

--- common/components/AboutMe.tsx
+++ common/components/AboutMe.tsx
@@ -10,48 +10,46 @@ import typeScriptIcon from "../../public/typescript.svg";
 import reactIcon from "../../public/react.svg";
 import mongoDbIcon from "../../public/mongodb.svg";
 import swiftIcon from "../../public/swift.svg";
-import { motion, useScroll } from "framer-motion";
+import { easeIn, motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
+const iconArray = [
+  { height: 200, width: 200, alt: "Express Icon", src: expressIcon },
+  { height: 50, width: 50, alt: "Framer Motion Icon", src: framerMotionIcon },
+  { height: 160, width: 160, alt: "Next Js Icon", src: nextJsIcon },
+  { height: 120, width: 120, alt: "nodejs Icon", src: nodeJsIcon },
+  { height: 300, width: 200, alt: "Tailwind Icon", src: tailwindCssIcon },
+  { height: 90, width: 90, alt: "TypeScript Icon", src: typeScriptIcon },
+  { height: 90, width: 90, alt: "React Icon", src: reactIcon },
+  { height: 180, width: 180, alt: "Mongo Db Icon", src: mongoDbIcon },
+  { height: 90, width: 90, alt: "Swift Icon", src: swiftIcon },
+];
+
 export default function AboutMe() {
-  const introductionRef = useRef(null);
-  const skillLogosRef = useRef(null);
-
-  const { scrollYProgress: scrollYProgressIntroduction } = useScroll({
-    target: introductionRef,
-    offset: ["start end", "start center"],
-  });
-
-  const { scrollYProgress: scrollYProgressSkillLogos } = useScroll({
-    target: skillLogosRef,
-    offset: ["start 0.9", "start center"],
-  });
-
   return (
     <>
-      <motion.div
-        ref={introductionRef}
-        style={{ opacity: scrollYProgressIntroduction }}
-        className="mb-32"
-      >
-        <Introduction />
-      </motion.div>
-
-      <motion.div
-        ref={skillLogosRef}
-        style={{ opacity: scrollYProgressSkillLogos }}
-      >
-        <SkillLogos />
-      </motion.div>
+      <Introduction />
+      <SkillIcons />
       <div className="h-screen"></div>
     </>
   );
 }
 
 export function Introduction() {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "start center"],
+  });
+
   return (
     <>
-      <p className={` text-white text-5xl text-center font-bold `}>
+      <motion.p
+        className={` text-white text-5xl text-center font-bold mb-32`}
+        ref={ref}
+        style={{ opacity: scrollYProgress }}
+      >
         I'm Tuğberk,
         <br /> An aspiring Full Stack developer
         <br /> who brings
@@ -60,73 +58,79 @@ export function Introduction() {
         </span>
         <br /> experiences that engages users and <br />
         makes a difference.
-      </p>
+      </motion.p>
     </>
   );
 }
 
-export function SkillLogos() {
+export function SkillIcons() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start 0.8", "center center"],
+  });
+
   return (
     <>
-      <div className="mr-4 ml-4 flex flex-col gap-20">
-        <div className="flex gap-20 w-full justify-center items-center  flex-wrap">
-          <Image
-            height={200}
-            width={200}
-            alt="Express Icon"
-            src={expressIcon}
-          ></Image>
-          <Image
-            style={{ width: 50, height: 50 }}
-            alt="Framer Motion Icon"
-            src={framerMotionIcon}
-          ></Image>
-          <Image
-            height={160}
-            width={160}
-            alt="Next Js Icon"
-            src={nextJsIcon}
-          ></Image>
-          <Image
-            height={120}
-            width={120}
-            alt="nodejs Icon"
-            src={nodeJsIcon}
-          ></Image>
+      <motion.div className="mr-4 ml-4 flex flex-col gap-20" ref={ref}>
+        <div className="flex gap-20 w-full justify-center items-center flex-wrap">
+          {iconArray.slice(0, 4).map((icon, i) => (
+            <Icon
+              key={icon.alt}
+              index={i}
+              total={iconArray.length}
+              height={icon.height}
+              width={icon.width}
+              alt={icon.alt}
+              src={icon.src}
+              scrollYProgress={scrollYProgress}
+            />
+          ))}
         </div>
-        <div className="flex gap-20 w-full justify-center items-center flex-wrap ">
-          <Image
-            height={300}
-            width={300}
-            alt="Tailwind Icon"
-            src={tailwindCssIcon}
-          ></Image>
-          <Image
-            height={90}
-            width={90}
-            alt="Type Script Icon"
-            src={typeScriptIcon}
-          ></Image>
-          <Image
-            height={90}
-            width={90}
-            alt="React Icon"
-            src={reactIcon}
-          ></Image>
-          <Image
-            height={180}
-            width={180}
-            alt="Mongo Db Icon"
-            src={mongoDbIcon}
-          ></Image>
-          <Image
-            height={90}
-            width={90}
-            alt="Swift Icon"
-            src={swiftIcon}
-          ></Image>
+        <div className="flex gap-20 w-full justify-center items-center flex-wrap">
+          {iconArray.slice(4).map((icon, i) => (
+            <Icon
+              key={icon.alt}
+              index={i + 4}
+              total={iconArray.length}
+              height={icon.height}
+              width={icon.width}
+              alt={icon.alt}
+              src={icon.src}
+              scrollYProgress={scrollYProgress}
+            />
+          ))}
         </div>
-      </div>
+      </motion.div>
     </>
+  );
+}
+
+type IconProps = {
+  height: number;
+  width: number;
+  alt: string;
+  src: any;
+  scrollYProgress: any;
+};
+
+export function Icon({
+  height,
+  width,
+  alt,
+  src,
+  index,
+  total,
+  scrollYProgress,
+}: IconProps & { index: number; total: number }) {
+  const start = index / total;
+  const end = start + 1 / total;
+
+  const opacity = useTransform(scrollYProgress, [start, end], [0.1, 1]);
+
+  return (
+    <motion.div style={{ opacity: opacity }}>
+      <Image height={height} width={width} alt={alt} src={src} />
+    </motion.div>
   );
 }
