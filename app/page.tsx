73aee80c9c0d9cbce21b7f194ee62da095f6de14
TugberkAta <atatugberk@yahoo.com>
2024-03-05 17:44:54 +0200

    Add lenis scroll smoothing to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import Introduction from "@/common/components/Introduction";
 import LightShow from "@/common/components/LightShow";
 import Navigation from "@/common/components/Navigation";
 import SkillIcons from "@/common/components/SkillIcons";
+import Lenis from "@studio-freight/lenis/types";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <div className="bg-gray-950 flex-col h-fit w-screen">
@@ -13,6 +28,7 @@ export default function Home() {
         </div>
         <Introduction></Introduction>
         <SkillIcons></SkillIcons>
+        <div className="h-screen"></div>
       </div>
     </>
   );
