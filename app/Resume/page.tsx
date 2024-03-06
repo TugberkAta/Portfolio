e04041b3de4db111c94855733dbb7663fe756d27
TugberkAta <atatugberk@yahoo.com>
2024-03-06 13:59:24 +0200

    Add a page for resume with emmbed pdf

---
+++ app/Resume/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Resume() {
+  return (
+    <>
+      <embed
+        className="h-full w-full h-screen"
+        src="https://drive.google.com/file/d/1lIe0ozSc8E2Bfrs1qYxL2TBUERp2vemV/preview?usp=sharing"
+        type=""
+      />
+    </>
+  );
+}
