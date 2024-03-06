3da215fc22712fc002fdc7652b929e0ff41de4fa
TugberkAta <atatugberk@yahoo.com>
2024-03-06 16:16:38 +0200

    Add a function to check if the user is on mobile device and reduce the width of triangle light animation depending on that

--- common/components/LightShow.tsx
+++ common/components/LightShow.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import useDeviceDetection from "../lib/useDeviceDetection";
 
 type LightShowProps = {
   count: number;
@@ -38,17 +39,20 @@ export function LightFragment({ count, reverse }: LightFragmentProps) {
   // Create state for the fragment array
   const [fragmentArr, setFragmentArr] = useState<FragmentArr[]>([]);
 
+  const device = useDeviceDetection(); // Checks the device the user is using
+
   // On render trigger the createLightFragment function
   useEffect(() => {
     createLightFragment(count);
-  });
+  }, [device]);
 
   function createLightFragment(amount: number) {
     const tempArr = []; // Create temp array to store the fragment objects
 
     // For loop to create the fragment objects and put them into fragmentArr state
     for (let index = amount; index > 0; index--) {
-      const width = (index + 1) * 20; // Width gets higher on every iteration
+      // Width gets higher on every iteration, if the user is using a mobile device the with starts from lower value
+      let width = device === "Desktop" ? (index + 1) * 20 : (index + 1) * 15;
       const transitionDelay = -index * 0.2; // Delay to keep them out of synch
 
       const fragment = {
