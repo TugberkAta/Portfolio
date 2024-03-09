c195d725ab0da2b518a736900340486e6bf772f7
TugberkAta <atatugberk@yahoo.com>
2024-03-09 16:03:45 +0200

    Add backToTopArrow component
    Reduce the margin between lightshow and introduction

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,34 @@ import Projects from "@/common/components/Projects";
 import SkillIcons from "@/common/components/SkillIcons";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import { nunito } from "@/common/styles/fonts";
+import BackToTopArrow from "@/common/components/BackToTopArrow";
+BackToTopArrow;
 
 export default function Home() {
   const lenis = useLenis(({ scroll }) => {});
 
   return (
-    <ReactLenis root>
-      <div className="bg-gray-950 flex-col h-fit w-screen">
-        <Navigation />
-        <div className="mt-20 mb-40 flex flex-col items-center">
-          <p
-            className={`text-white text-center text-3xl mb-4 font-bold ${nunito.className}`}
-          >
-            Crafting Excellence, Code by Code.
-          </p>
-          <LightShow count={20} />
+    <div>
+      <ReactLenis root>
+        <div className="bg-gray-950 flex-col h-fit w-screen">
+          <Navigation />
+          <div className="mt-20 mb-20 flex flex-col items-center">
+            <p
+              className={`text-white text-center text-3xl mb-4 font-bold ${nunito.className}`}
+            >
+              Crafting Excellence, Code by Code.
+            </p>
+            <LightShow count={20} />
+          </div>
+          <Introduction></Introduction>
+          <SkillIcons></SkillIcons>
+          <div className="mt-40 mb-20">
+            <Projects></Projects>
+          </div>
+          <Conclusion></Conclusion>
         </div>
-        <Introduction></Introduction>
-        <SkillIcons></SkillIcons>
-        <div className="mt-40 mb-20">
-          <Projects></Projects>
-        </div>
-        <Conclusion></Conclusion>
-      </div>
-    </ReactLenis>
+      </ReactLenis>
+      <BackToTopArrow></BackToTopArrow>
+    </div>
   );
 }
