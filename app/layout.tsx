54be359041e126d11a82788ce9be52ba6926a778
TugberkAta <atatugberk@yahoo.com>
2024-04-04 15:12:03 +0300

    Remove typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
-        <Analytics />{" "}
+        <Analytics />
       </body>
     </html>
   );
